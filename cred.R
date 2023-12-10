e30eed71c372c69bc6bce65806370a909ea81069
[jakobrk95] <[jakobrk95@gmail.com]>
2023-12-10 20:29:56 +0000

    new key

--- cred.R
+++ cred.R
@@ -1,4 +1,4 @@
-key <- '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'
+key <- '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'
 
 API_key = '73ba61b5cbmsh2bba8a1a5fc2320p1f608ejsnaaa23d0ae76d'
 API_host = 'alpha-vantage.p.rapidapi.com'
