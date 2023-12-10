32467df78ea05284622442c926876ac1d95737ea
[jakobrk95] <[jakobrk95@gmail.com]>
2023-12-10 20:34:39 +0000

    new key

--- cred.R
+++ cred.R
@@ -1,4 +1,4 @@
-key <- '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'
+key <- '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'
 
 API_key = '73ba61b5cbmsh2bba8a1a5fc2320p1f608ejsnaaa23d0ae76d'
 API_host = 'alpha-vantage.p.rapidapi.com'
