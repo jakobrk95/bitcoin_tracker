a4aa8e118e1311a99e5fe1d92e40f8c91c93c180
[jakobrk95] <[jakobrk95@gmail.com]>
2023-12-10 15:02:51 +0000

    Scraper code updated

--- scraper.R
+++ scraper.R
@@ -1,2 +1,15 @@
 library(httr2)
+source('PAT.R')
 
+req <- request("https://alpha-vantage.p.rapidapi.com/query") %>%
+  req_url_query(  "from_currency" = "BTC",
+                  "function" = "CURRENCY_EXCHANGE_RATE",
+                  "to_currency" = "USD") %>%
+  req_headers('X-RapidAPI-Key' = API_key,
+              'X-RapidAPI-Host' = API_host) 
+resp <- req %>% 
+  req_perform() 
+
+price_str = resp_body_json(resp)$`Realtime Currency Exchange Rate`$`9. Ask Price`
+price = as.numeric(price_str)
+price
